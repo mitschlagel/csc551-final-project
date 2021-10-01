d5a416c18468b2406ab0db087b580c822a7c1f90
Spencer Jones <spencerjones@creighton.edu>
2021-09-30 23:15:13 -0500

    Add footer styling and content More attempts at getting tables to work

--- js/tables.js
+++ js/tables.js
@@ -1,16 +1,33 @@
 function displayTable() {
   const league = document.querySelector("#league").value;
 
+  const tableToDisplay = document.createElement("article");
+
   const tableContainer = document.querySelector(".table-main");
   tableContainer.innerHTML = "";
-  const table = document.createElement("div");
-  table.classList.add("league-table");
-  tableContainer.appendChild(table);
-  table.innerHTML = league;
+  tableContainer.appendChild(tableToDisplay);
+
+  console.log(league);
+  tableToDisplay.innerHTML = `
+  <div
+  id="wg-api-football-standings"
+  data-host="v3.football.api-sports.io"
+  data-league="${league}"
+  data-team=""
+  data-season="2021"
+  data-key="569fd3056fbfd09a47a568e3b82163f7"
+  data-theme=""
+  data-show-errors="false"
+  class="api_football_loader"
+></div>
+<script
+      type="module"
+      src="https://widgets.api-sports.io/football/1.1.8/widget.js"
+    ></script>
+
+  `;
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  document
-    .querySelector("select")
-    .addEventListener("input", () => displayTable());
-});
+document
+  .querySelector("#table-select-button")
+  .addEventListener("click", () => displayTable());
