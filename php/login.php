1bb5f6bb863d586d0e7f862d4fed0d473a87aa1b
DiegoRojo99 <diego99rojo@gmail.com>
2021-11-10 15:27:25 -0600

    Login checked data in the database

--- php/login.php
+++ php/login.php
@@ -13,5 +13,38 @@ Your username: <?php print($_POST['username']); ?> </br>
 Password: <?php print($_POST['password']); ?> </br>
 </p>
 
+
+<?php 
+
+$user=$_POST['username'];
+$pass=$_POST['password'];
+
+include("connectToDB.inc");
+$dataBase = connectDB();
+$query='SELECT * FROM users;';
+$result=mysqli_query($dataBase,$query) or die('Query failed: '.mysqli_error($dataBase));
+   $loggedIn=false;
+
+   while ($row = mysqli_fetch_array($result, MYSQL_ASSOC))
+   {
+   extract($row); //so we can use $ord_id and $cust_id
+
+      if($Username==$user && $Password==$pass){
+         echo "<p> Username: $Username <br/>
+            Email: $Email <br/>
+            Phone: $Phone <br/>
+            BirthDate: $BirthDate </p>";
+         $loggedIn=true;
+      } 
+      
+   }
+
+   if($loggedIn==false){
+      echo "<p> The email or password are incorrect </p>";
+   }
+
+mysql_close($dataBase);
+?>
+
 </body>
 </html>
