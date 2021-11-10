d133f20db1c96b2a039617406abc5d8a664274cd
DiegoRojo99 <diego99rojo@gmail.com>
2021-11-10 13:10:04 -0600

    It pushes the register info into the database

--- php/register.php
+++ php/register.php
@@ -18,7 +18,22 @@ Your registration time is:
 <?php 
 date_default_timezone_set('US/Central');
 $date_and_time = date("l, F d Y g:i:s a");
-print($date_and_time); ?>
+print($date_and_time); ?></br>
+
+<?php
+$username=$_POST['username'];
+$password=$_POST['password'];
+$email=$_POST['email'];
+$phone=$_POST['phone'];
+$birthdate=$_POST['birthdate'];
+ 
+$dbConnection=mysqli_connect("sql304.epizy.com","epiz_29507824","TKiNcSS1UvTF9V", "epiz_29507824_soccer");
+$q1='INSERT INTO users VALUES("';
+$q2='","';
+$q3='");';
+$query=$q1.$username.$q2.$password.$q2.$email.$q2.$phone.$q2.$birthdate.$q3;
+$result=mysqli_query($dbConnection,$query) or die('Query failed: '.mysqli_error($dbConnection))
+?>
 </p>
 
 </body>
